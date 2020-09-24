1611abfe7914fad794a9a19269f5ae4532f98867
Ofek <tupe12334@gmail.com>
2020-09-25 00:27:33 +0300

    Updates

---
+++ precache-manifest.57f88a7cf09bddb9b90c248647f56fc3.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "63f8482ef35466ebd021e8b70573d484",
+    "url": "/vitamins/index.html"
+  },
+  {
+    "revision": "cfb14bf855c1795fb10a",
+    "url": "/vitamins/static/css/2.40b8b162.chunk.css"
+  },
+  {
+    "revision": "27846111df2ac6b6e11c",
+    "url": "/vitamins/static/css/main.523f09c5.chunk.css"
+  },
+  {
+    "revision": "cfb14bf855c1795fb10a",
+    "url": "/vitamins/static/js/2.de284ade.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/vitamins/static/js/2.de284ade.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "27846111df2ac6b6e11c",
+    "url": "/vitamins/static/js/main.1fa42b66.chunk.js"
+  },
+  {
+    "revision": "2e2553ba9e043374fd9a",
+    "url": "/vitamins/static/js/runtime-main.202c0da6.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/vitamins/static/media/logo.ee7cd8ed.svg"
+  },
+  {
+    "revision": "c762d850e2e8d0e29047608715196736",
+    "url": "/vitamins/static/media/roboto-all-400-italic.c762d850.woff"
+  },
+  {
+    "revision": "a91ad097d24828af724d4fee36a063ed",
+    "url": "/vitamins/static/media/roboto-all-400-normal.a91ad097.woff"
+  },
+  {
+    "revision": "8266321ab43353bcd147ad67600d165c",
+    "url": "/vitamins/static/media/roboto-cyrillic-400-italic.8266321a.woff2"
+  },
+  {
+    "revision": "8bb64952764a884d67019b3486296ab9",
+    "url": "/vitamins/static/media/roboto-cyrillic-400-normal.8bb64952.woff2"
+  },
+  {
+    "revision": "b3e580d221a4722c959155878ab94210",
+    "url": "/vitamins/static/media/roboto-cyrillic-ext-400-italic.b3e580d2.woff2"
+  },
+  {
+    "revision": "4743c758a952f2bd4a35d4e42afc002b",
+    "url": "/vitamins/static/media/roboto-cyrillic-ext-400-normal.4743c758.woff2"
+  },
+  {
+    "revision": "469a78405c3ae073ba769321fa0584f3",
+    "url": "/vitamins/static/media/roboto-greek-400-italic.469a7840.woff2"
+  },
+  {
+    "revision": "c1e9793c84cb26c44ef2a2cf8b6f49ce",
+    "url": "/vitamins/static/media/roboto-greek-400-normal.c1e9793c.woff2"
+  },
+  {
+    "revision": "801b64f75ae26dab9c36f00629ebdad0",
+    "url": "/vitamins/static/media/roboto-greek-ext-400-italic.801b64f7.woff2"
+  },
+  {
+    "revision": "182ee6a4872ca8fa78048951b1561a5c",
+    "url": "/vitamins/static/media/roboto-greek-ext-400-normal.182ee6a4.woff2"
+  },
+  {
+    "revision": "51521a2a8da71e50d871ac6fd2187e87",
+    "url": "/vitamins/static/media/roboto-latin-400-italic.51521a2a.woff2"
+  },
+  {
+    "revision": "479970ffb74f2117317f9d24d9e317fe",
+    "url": "/vitamins/static/media/roboto-latin-400-normal.479970ff.woff2"
+  },
+  {
+    "revision": "dc3871f486e189164db4b98968330bc4",
+    "url": "/vitamins/static/media/roboto-latin-ext-400-italic.dc3871f4.woff2"
+  },
+  {
+    "revision": "455200cb007fe1212c668721d827c691",
+    "url": "/vitamins/static/media/roboto-latin-ext-400-normal.455200cb.woff2"
+  },
+  {
+    "revision": "9780bde87bceec579eaf16bddad47615",
+    "url": "/vitamins/static/media/roboto-vietnamese-400-italic.9780bde8.woff2"
+  },
+  {
+    "revision": "a8be5b46d06bb541b0968196ee5e6bb8",
+    "url": "/vitamins/static/media/roboto-vietnamese-400-normal.a8be5b46.woff2"
+  }
+]);
