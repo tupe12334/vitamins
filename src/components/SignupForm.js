1242b3abd903673ecc98ca4cd90290085157b169
Ofek <tupe12334@gmail.com>
2020-09-25 00:28:01 +0300

    upload

--- src/components/SignupForm.js
+++ src/components/SignupForm.js
@@ -1,18 +1,54 @@
 import React from "react";
-import TextField from '@material-ui/core/TextField';
-import Button from '@material-ui/core/Button';
+import { withStyles } from '@material-ui/core/styles';
+import { Typography, Button, Radio, RadioGroup, FormControl, FormControlLabel, TextField } from '@material-ui/core';
+import { blue, green, orange, pink, red } from '@material-ui/core/colors';
 
+import 'fontsource-roboto';
+
+const BlueRadio = withStyles({
+    root: {
+        color: blue[400],
+        '&$checked': {
+            color: blue[600],
+        },
+    },
+    checked: {},
+})((props) => <Radio color="default" {...props} />);
+const PinkRadio = withStyles({
+    root: {
+        color: pink[400],
+        '&$checked': {
+            color: pink[600],
+        },
+    },
+    checked: {},
+})((props) => <Radio color="default" {...props} />);
 
 
 export default function SignupForm() {
-    const [UserName, setUserName] = React.useState(null)
-    const saveName = () => {
+    const [UserName, setUserName] = React.useState(localStorage.getItem('UserName') || null)
+    const [UserGender, setUserGender] = React.useState(localStorage.getItem('UserGender'))
+    const saveUserData = () => {
         localStorage.setItem('UserName', UserName);
+        localStorage.setItem('UserGender', UserGender);
     }
+
+    const handleChange = (event) => {
+        setUserGender(event.target.value);
+
+    };
     const onChange = event => setUserName(event.target.value);
-  return <form dir="rtl">
-      <TextField label="שם מלא" value={UserName} onChange={onChange}/>
-      <br/>
-      <Button variant="contained" onClick={saveName}>הרשם</Button>
-  </form>
+    return <FormControl dir="rtl">
+        <Typography variant="h3">טופס שמירת נתונים אישיים</Typography>
+        <br />
+        <br />
+        <TextField label="שם מלא" value={UserName} onChange={onChange} />
+        <br />
+        <RadioGroup aria-label="gender" name="gender1" value={UserGender} onChange={handleChange}>
+            <FormControlLabel value="זכר" control={<BlueRadio />} label="זכר" />
+            <FormControlLabel value="נקבה" control={<PinkRadio />} label="נקבה" />
+        </RadioGroup>
+        <br />
+        <Button variant="contained" onClick={saveUserData}>שמור נתונים</Button>
+    </FormControl>
 }
